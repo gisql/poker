a23e5022c75b48090bb2259bd177b38cdb14106d
Cyril Lapinte <cyril.lapinte@gmail.com>
2014-05-18 13:46:15 -0700

    Adding a simple behaviour to BasicPlayer

--- player/src/main/java/poker/player/cyril/BasicPlayer.java
+++ player/src/main/java/poker/player/cyril/BasicPlayer.java
@@ -1,7 +1,8 @@
 package poker.player.cyril;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 import poker.MoveDTO;
-import poker.MoveDTO.MoveType;
 import poker.Player;
 import poker.PlayerStateDTO;
 import poker.TableDTO;
@@ -13,37 +14,32 @@ import poker.TerminationListener;
 /**
  * Basic Player Implementation
  * 
- * Aim of this class :
- *    - Be as simple as possible but still a valid player
- *    - Validate test case with the set of minimal constraints
- *    - Discover the Poker API
+ * Aim of this class : - Be as simple as possible but still a valid player -
+ * Validate test case with the set of minimal constraints - Discover the Poker
+ * API
  **/
 public class BasicPlayer implements Player, TableListener, TerminationListener {
+    
+    public BasicPlayer() {
+    }
 
-	public BasicPlayer() {
-		
-	}
+    public BasicPlayer(String string) {
+    }
 
-	public BasicPlayer(String string) {
-		
-	}
-	
-	@Override
-	public void terminatedBy(TerminationEvent event) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void terminatedBy(TerminationEvent event) {
+    }
 
-	@Override
-	public void tableChanged(TableEvent event) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void tableChanged(TableEvent event) {
+    }
 
-	@Override
-	public MoveDTO makeMove(PlayerStateDTO pi, TableDTO table) {
-		// Ok, he is very basic but he's also a looser! ;-)
-		return MoveDTO.FOLD;
-	}
-
+    @Override
+    public MoveDTO makeMove(PlayerStateDTO pi, TableDTO table) {
+        if(table.getChipsToCall() < pi.getChipsLeft()) {
+            return MoveDTO.CALL;
+        }
+        return MoveDTO.FOLD;
+    }
+    
 }
